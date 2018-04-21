d07fbb0955a15fdde13ae5c35c317df5f66f5f25
Simon Hoye <simonhoye@gmail.com>
2018-04-21 11:35:28 +1000

    Added linting and more tests

--- app/app.js
+++ app/app.js
@@ -1,30 +1,27 @@
 const express = require('express');
 const AWS = require('aws-sdk');
 
-process.env.AWS_SAM_LOCAL && AWS.config.update({
- region: "ap-southeast-2",
- endpoint: "http://docker.for.mac.host.internal:8888"
-});
+if (process.env.AWS_SAM_LOCAL) {
+  AWS.config.update({
+    region: 'ap-southeast-2',
+    endpoint: 'http://docker.for.mac.host.internal:8888',
+  });
+}
 
-const dc = new AWS.DynamoDB.DocumentClient();
 const app = express();
-const table = "apptable";
+const table = 'apptable';
+const params = {
+  TableName: table,
+};
 
-app.get("/test", (req, res) => {
-  let params = {
-    TableName: table
-  };
+app.get('/ping', (req, res) => res.status(200).send({ result: 'pong' }));
+app.get('/test', (req, res) => {
+  const dc = new AWS.DynamoDB.DocumentClient();
   dc
     .scan(params)
     .promise()
-    .then(response => {
-      res.status(200).send(response);
-    })
-    .catch(err => {
-      res.status(500).send(err);
-    });
+    .then(response => res.status(200).send(response))
+    .catch(err => res.status(500).send(err));
 });
-app.get("/ping", (req, res) => {
-  res.status(200).send({result: 'pong'});
-});
+
 module.exports = app;
--- app/app.test.js
+++ app/app.test.js
@@ -1,13 +1,36 @@
-const supertest = require("supertest");
-const app = require("./app");
+const AWS = require('aws-sdk-mock');
+const supertest = require('supertest');
+const app = require('./app');
 
 const request = supertest(app);
 
 describe('GET /ping', () => {
-    it('should return pong', done => {
-      request
-        .get('/ping')
-        .expect('content-type', 'application/json; charset=utf-8')
-        .expect(200, {result: 'pong'}, done);
+  it('should return successfully with pong', (done) => {
+    request
+      .get('/ping')
+      .expect('content-type', 'application/json; charset=utf-8')
+      .expect(200, { result: 'pong' }, done);
+  });
+});
+describe('GET /test', () => {
+  it('should return successfully with the DynamoDB response', (done) => {
+    const mockResponse = { Items: [], Count: 0, ScannedCount: 0 };
+    AWS.mock('DynamoDB.DocumentClient', 'scan', (
+      params,
+      callback,
+    ) => {
+      callback(null, mockResponse);
     });
+    request
+      .get('/test')
+      .expect('content-type', 'application/json; charset=utf-8')
+      .expect(200, mockResponse, done);
+  });
+  it('should return an error', (done) => {
+    request
+      .get('/test')
+      .expect('content-type', 'application/json; charset=utf-8')
+      .expect(500, done);
+  });
+  afterEach(() => AWS.restore());
 });
